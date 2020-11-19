28fdb48bf3dde1900a835a5ce7d1d5c29ff22941
salimalsulaimi <66360827+salimalsulaimi@users.noreply.github.com>
2020-11-19 14:51:52 +0400

    ..

--- animation with css/2/script.js
+++ animation with css/2/script.js
@@ -1,26 +1,25 @@
 let footer = document.querySelector('.footer'),
     ioBnLike = document.querySelectorAll('.io-button-like'),
+    ioBnComment = document.querySelectorAll('.io-button-comment'),
+    ioBnSend = document.querySelectorAll('.io-button-send'),
+    ioBnBookmark = document.querySelectorAll('.io-button-bookmark'),
     footerLike = document.querySelector('.footer-like');
 
+
+// like :
 footerLike.classList.remove("io-footer-like");
-
 let current = 0;
-
-ioBnLike.forEach(likeItem => {
-    likeItem.onclick = _ => {
-        likeItem.classList.toggle('far');
-        likeItem.classList.toggle('fas');
-        likeItem.classList.toggle('like');
+ioBnLike.forEach(element => {
+    element.addEventListener('click', _ => {
+        ioColor(element, 'like');
 
         footerLike.classList.add('io-footer-like');
 
-        if (likeItem.classList.contains('like')) {
+        if (element.classList.contains('like')) {
             current++;
+            creatNoticeLike();
         }
-
-        creatNoticeLike();
-
-    }
+    });
 });
 
 function creatNoticeLike() {
@@ -33,4 +32,86 @@ function creatNoticeLike() {
     let noticeLike = document.querySelector('.notice-like');
         noticeLike.remove();
     }, 5000);
+}
+
+
+// comment :
+ioBnComment.forEach(element => {
+    let cardComment = document.querySelector('.card-comment');
+
+
+    element.addEventListener('click', e => {
+        ioColor(element, 'comment');
+        cardComment.classList.toggle('open');
+
+        e.stopImmediatePropagation();
+
+        cardComment.addEventListener('click', e => {
+            e.stopImmediatePropagation();
+        });
+
+        window.addEventListener('click', ele => {
+            if (ele !== cardComment && ele !== element) {
+                cardComment.classList.remove('open');
+                element.classList.add('far');
+                element.classList.remove('fas');
+                element.classList.remove('send');
+            }
+        });
+    });
+});
+
+
+// send :
+ioBnSend.forEach(element => {
+    let cardSend = document.querySelector('.card-send');
+
+    element.addEventListener('click', e => {
+        ioColor(element, 'send');
+
+        if (element.classList.contains('send')) {
+
+            cardSend.classList.toggle('open');
+
+            e.stopImmediatePropagation();
+
+            cardSend.addEventListener('click', e => {
+                e.stopImmediatePropagation();
+            });
+
+            window.addEventListener('click', ele => {
+                if (ele !== cardSend && ele !== element) {
+                    cardSend.classList.remove('open');
+                    element.classList.add('far');
+                    element.classList.remove('fas');
+                    element.classList.remove('send');
+                }
+            });
+        }
+    });
+});
+
+
+// bookmark :
+ioBnBookmark.forEach(element => {
+    let imgFile = document.querySelector('.img-file');
+
+    element.addEventListener('click', _ => {
+        ioColor(element, 'bookmark');
+
+        if (element.classList.contains('bookmark')) {
+            imgFile.classList.toggle('scale');
+            setTimeout( _ => {
+
+                imgFile.classList.remove('scale');
+
+            }, 4000);
+        }
+    })
+});
+
+function ioColor (element, classColor) {
+    element.classList.toggle('far');
+    element.classList.toggle('fas');
+    element.classList.toggle(classColor);
 }
